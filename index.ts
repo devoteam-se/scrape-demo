91e749c42424b6dbf1a0bd53db10f6c6f0b0f98e
Jonas Stenberg <github@stenberg.io>
2024-02-07 13:47:04 +0100

    Initial commit

---
+++ index.ts
@@ -0,0 +1,38 @@
+import puppeteer, { ElementHandle } from 'puppeteer'
+
+const browser = await puppeteer.launch({
+  headless: false,
+  args: [`--window-size=1920,1080`],
+  defaultViewport: {
+    width: 1920,
+    height: 1080
+  }
+})
+const page = await browser.newPage()
+
+await page.goto('https://www.ikea.com/se/sv/cat/plantor-blommor-pp003/', {
+  waitUntil: 'networkidle2'
+})
+
+const links = await page.evaluate(() =>
+  Array.from(document.querySelectorAll('a.plp-product__image-link.link')).map((e) => (e as HTMLAnchorElement).href)
+)
+
+console.log(links)
+
+const articleNumbers = {}
+
+for (const link of links) {
+  await page.goto(link, {
+    waitUntil: 'networkidle2'
+  })
+  const articleNumber = await page.evaluate(
+    () => document.querySelector('span[class="pip-product-identifier__value"]')?.textContent
+  )
+
+  articleNumbers[link] = articleNumber
+}
+
+console.log(articleNumbers)
+
+await browser.close()
